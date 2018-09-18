45153199e5b93ce462859484bcac0805b49e542c
Vitaly Dmitriev <vitalik.dmitriev1995@gmail.com>
2018-09-19 00:34:06 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var delete_space = equation.replace(/\s/g, "");
+  var result = delete_space.match(/\(+|-?[^\^][\d]+/g);
+  var D = Math.pow(result[1], 2) - 4*result[0]*result[2];
+  if(D>0){
+    var x1 = (-result[1] + Math.sqrt(D))/(2*result[0]);
+    var x2 = (-result[1] - Math.sqrt(D))/(2*result[0]);
+    return [Math.round(x1),Math.round(x2)].sort(compareNumbers);
+  }
+  else if(D==0){
+    var x = -result[1]/(2*result[0]);
+    return [x];
+  }
+  else{
+    return NULL;
+  }
+  function compareNumbers(a, b) {
+    return a - b;
+  }
 }
